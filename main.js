e4bd5b590702ce66f7d74a6f4fd468852f50596d
Erik Gillespie <erik.gillespie@gmail.com>
2019-03-06 15:33:45 -0500

    Function to draw horizontal doors

--- main.js
+++ main.js
@@ -2,14 +2,33 @@ const wait = async (ms) => {
   return new Promise((resolve) => {
     setTimeout(resolve, ms)
   })
-}  
+}
 
-const addDoor = async ({svg, x, y}) => {
-  const path = document.createElementNS(
+const createPath = () => {
+  return document.createElementNS(
     'http://www.w3.org/2000/svg',
     'path'
   )
+}
 
+const drawHorizontalDoor = async ({svg, x, y}) => {
+  const path = createPath()
+  path.setAttributeNS(null, 'class', 'door')
+  path.setAttributeNS(null, 'd', `
+    M ${x},${y}
+    v -1
+    h 8
+    v 2
+    h -8
+    v -1
+  `)
+
+  svg.appendChild(path)
+  await wait(1000)
+}
+
+const drawVerticalDoor = async ({svg, x, y}) => {
+  const path = createPath()
   path.setAttributeNS(null, 'class', 'door')
   path.setAttributeNS(null, 'd', `
     M ${x},${y}
@@ -26,13 +45,15 @@ const addDoor = async ({svg, x, y}) => {
 
 (async () => {
   const dungeon = document.getElementById('dungeon')
-  await addDoor({ svg: dungeon, x: 40, y: 61 })
-  await addDoor({ svg: dungeon, x: 80, y: 21 })
-  await addDoor({ svg: dungeon, x: 80, y: 101 })
-  await addDoor({ svg: dungeon, x: 100, y: 51 })
-  await addDoor({ svg: dungeon, x: 100, y: 71 })
-  await addDoor({ svg: dungeon, x: 100, y: 91 })
-  await addDoor({ svg: dungeon, x: 100, y: 111 })
-  await addDoor({ svg: dungeon, x: 10, y: 57 })
-  await addDoor({ svg: dungeon, x: 10, y: 65 })
+  await drawHorizontalDoor({ svg: dungeon, x: 21, y: 40 })
+  await drawHorizontalDoor({ svg: dungeon, x: 21, y: 90 })
+  await drawVerticalDoor({ svg: dungeon, x: 40, y: 61 })
+  await drawVerticalDoor({ svg: dungeon, x: 80, y: 21 })
+  await drawVerticalDoor({ svg: dungeon, x: 80, y: 101 })
+  await drawVerticalDoor({ svg: dungeon, x: 100, y: 51 })
+  await drawVerticalDoor({ svg: dungeon, x: 100, y: 71 })
+  await drawVerticalDoor({ svg: dungeon, x: 100, y: 91 })
+  await drawVerticalDoor({ svg: dungeon, x: 100, y: 111 })
+  await drawVerticalDoor({ svg: dungeon, x: 10, y: 57 })
+  await drawVerticalDoor({ svg: dungeon, x: 10, y: 65 })
 })()
