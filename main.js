9b07f462f2fa625dc1f8ba6cbf179c994cbba735
Erik Gillespie <erik.gillespie@gmail.com>
2019-03-05 21:28:22 -0500

    Use JavaScript to add doors and control animation delays

---
+++ main.js
@@ -0,0 +1,35 @@
+const wait = async (ms) => {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms)
+  })
+}  
+
+const addDoor = async ({svg, x, y}) => {
+  const path = document.createElementNS(
+    'http://www.w3.org/2000/svg',
+    'path'
+  )
+
+  path.setAttributeNS(null, 'class', 'door')
+  path.setAttributeNS(null, 'd', `
+    M ${x},${y}
+    h 1
+    v 8
+    h -2
+    v -8
+    h 1
+  `)
+
+  svg.appendChild(path)
+  await wait(1000)
+}
+
+(async () => {
+  const dungeon = document.getElementById('dungeon')
+  await addDoor({ svg: dungeon, x: 100, y: 51 })
+  await addDoor({ svg: dungeon, x: 100, y: 71 })
+  await addDoor({ svg: dungeon, x: 100, y: 91 })
+  await addDoor({ svg: dungeon, x: 100, y: 111 })
+  await addDoor({ svg: dungeon, x: 10, y: 57 })
+  await addDoor({ svg: dungeon, x: 10, y: 65 })
+})()
