111669edc2442338186359495ab8351600c7e190
Erik Gillespie <erik.gillespie@gmail.com>
2019-03-09 21:45:04 -0500

    Moved stroke and fill styles into path drawer so they get downloaded with the SVG

--- src/drawers/path-single.js
+++ src/drawers/path-single.js
@@ -1,12 +1,24 @@
 import createPath from '/src/svg/create-path'
 import waitForPathToDraw from '/src/helpers/wait-for-path-to-draw'
 
-export default async ({svg, style, d}) => {
+export default async ({
+  svg,
+  d,
+  style,
+  fill = 'none',
+  stroke = '#2b2726',
+  strokeLinecap = 'square',
+  strokeWidth = 1
+}) => {
   const path = createPath()
+  path.setAttributeNS(null, 'd', d)
   if (style && style.length) {
     path.setAttributeNS(null, 'class', style)
   }
-  path.setAttributeNS(null, 'd', d)
+  path.setAttributeNS(null, 'fill', fill)
+  path.setAttributeNS(null, 'stroke', stroke)
+  path.setAttributeNS(null, 'stroke-linecap', strokeLinecap)
+  path.setAttributeNS(null, 'stroke-width', strokeWidth)
   svg.appendChild(path)
   await waitForPathToDraw()
 }
