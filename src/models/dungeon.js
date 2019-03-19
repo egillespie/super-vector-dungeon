08f09a9f73ed85cd93b757b6b82e126d3dd37738
Erik Gillespie <erik.gillespie@gmail.com>
2019-03-18 22:22:20 -0400

    Create randomly-oriented entrance

--- src/models/dungeon.js
+++ src/models/dungeon.js
@@ -1,10 +1,20 @@
+/* eslint-disable no-console */
 import { fromJS } from 'immutable'
 import oneDoorCell from './rooms/cell-doors-1'
 import createRngFromSeed from '/src/helpers/rng/create-rng-from-seed'
+import rotateRoomCW90InPlace from '/src/transformers/room-rotate-clockwise-90-in-place'
+import getRandomIntInRange from '/src/helpers/rng/get-random-int-in-range'
 
 export default ({ rngSeed }) => {
   const rng = createRngFromSeed(rngSeed)
-  const entrance = oneDoorCell()
+
+  // Create entrance and randomly rotate it
+  let entrance = oneDoorCell()
+  const rotations = getRandomIntInRange({ rng, max: 4 })
+  for (let i = 0; i < rotations; i++) {
+    entrance = rotateRoomCW90InPlace(entrance)
+  }
+
   return fromJS({
     rng,
     rngSeed,
