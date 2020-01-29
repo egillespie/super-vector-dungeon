8888b8784114ccbbea57e262728f267bb51a4f4e
Erik Gillespie <erik.gillespie@gmail.com>
2020-01-28 21:53:18 -0500

    Explain how to add a draw command when the translator is missing

--- src/translators/draw-command-to-string.js
+++ src/translators/draw-command-to-string.js
@@ -9,5 +9,20 @@ const drawCommandTranslators = Object.freeze({
 export default (drawCommand) => {
   const command = drawCommand.get('command')
   const drawCommandToString = drawCommandTranslators[command]
+  if (!drawCommandToString) {
+    const keys = Object.keys(drawCommand)
+    const index = keys.indexOf('command')
+    if (index >= 0) {
+      keys.splice(index, 1)
+    }
+
+    // eslint-disable-next-line no-console
+    console.error(
+      `Unrecognized draw command "${command}" ` +
+      `with properties ${keys}. ` +
+      'Did you write a translator in src/svg/draw-commands/ ' +
+      'and register it in draw-command-to-string.js?'
+    )
+  }
   return drawCommandToString(drawCommand)
 }
